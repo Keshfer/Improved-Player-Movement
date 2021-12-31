736390ce22c9251e8f0e3b14ac9fa75866887a95
Keshfer <Keshfur@gmail.com>
2021-12-30 18:19:44 -0800

    Initial commit

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraLook : MonoBehaviour
+{
+    private Transform mainCamera;
+    private Vector2 mouseDelta;
+    private float rotX;
+    private float rotY;
+    [Range(1.00f, 10.00f)]
+    public float sensitivity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = transform.Find("Player Cam");
+        rotX = transform.rotation.y;
+        rotY = transform.rotation.x;
+        
+
+    }
+
+    
+
+    //Rotates camera based on mouse movement from "Camera" action
+    public void look(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            mouseDelta = context.ReadValue<Vector2>();
+            rotX += mouseDelta.x * sensitivity * Time.deltaTime;
+            rotY -= mouseDelta.y * sensitivity * Time.deltaTime;
+            rotY = Mathf.Clamp(rotY, -90, 90);
+            gameObject.transform.localEulerAngles = new Vector3(transform.rotation.x, rotX, transform.rotation.z);
+            mainCamera.transform.localEulerAngles = new Vector3(rotY, mainCamera.transform.rotation.y, mainCamera.transform.rotation.z);
+
+        }
+
+    }
+    
+}
